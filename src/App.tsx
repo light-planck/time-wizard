f8e3a92e8736010760243a014635888fabb9e024
light-planck <seseifunc16@gmail.com>
2024-02-03 12:49:51 +0900

    feat: 追加・リセット機能の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,48 @@
-function App() {
-  return <div>app</div>;
-}
+import { useLocalStorage } from "usehooks-ts";
+import { v4 as uuidv4 } from "uuid";
+
+import { Interval } from "./types";
+
+const App = () => {
+	const [intervals, setIntervals] = useLocalStorage<Interval[]>(
+		"intervals",
+		[],
+	);
+
+	const handleAddInterval = () => {
+		setIntervals([...intervals, { id: uuidv4(), start: "", end: "" }]);
+	};
+
+	const handleResetIntervals = () => {
+		setIntervals([]);
+	};
+
+	return (
+		<div>
+			<div>
+				{intervals.map((interval) => (
+					<div key={interval.id}>
+						<form>
+							<label>開始</label>
+							<input type="time" value={interval.start} />
+						</form>
+						<form>
+							<label>終了</label>
+							<input type="time" value={interval.end} />
+						</form>
+					</div>
+				))}
+			</div>
+			<div>
+				<button type="button" onClick={handleAddInterval}>
+					追加
+				</button>
+				<button type="button" onClick={handleResetIntervals}>
+					リセット
+				</button>
+			</div>
+		</div>
+	);
+};
 
 export default App;
