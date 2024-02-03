ce85e927ab570a706c8ffdc0a6e65c44ff9dd72c
light-planck <seseifunc16@gmail.com>
2024-02-04 01:25:09 +0900

    style: format

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,11 @@
 export const Header = () => {
-	return (
-		<div>
-			<div className="p-5">
-				<h1 className="text-2xl">Add Time Intervals</h1>
-				<p>時間の区間の総和を計算できます。</p>
-			</div>
-			<hr />
-		</div>
-	);
+  return (
+    <div>
+      <div className="p-5">
+        <h1 className="text-2xl">Add Time Intervals</h1>
+        <p>時間の区間の総和を計算できます。</p>
+      </div>
+      <hr />
+    </div>
+  );
 };
