3a6f4f3aef41fb52b1818a1ac43f3cbc8825fec2
light-planck <seseifunc16@gmail.com>
2024-02-03 13:13:29 +0900

    feat: 計算機能の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { useLocalStorage } from "usehooks-ts";
 import { v4 as uuidv4 } from "uuid";
 
+import { useState } from "react";
 import { Interval } from "./types";
+import { Total } from "./types/total";
 
 const App = () => {
 	const [intervals, setIntervals] = useLocalStorage<Interval[]>(
 		"intervals",
 		[],
 	);
+
+	const handleChangeInterval = (id: string, newValue: Interval) => {
+		setIntervals(
+			intervals.map((interval) => (interval.id === id ? newValue : interval)),
+		);
+	};
 
 	const handleAddInterval = () => {
 		setIntervals([...intervals, { id: uuidv4(), start: "", end: "" }]);
@@ -17,6 +25,21 @@ const App = () => {
 		setIntervals([]);
 	};
 
+	const [total, setTotal] = useState<Total>({ hours: 0, minutes: 0 });
+
+	const handleAddIntervals = () => {
+		const total = intervals.reduce((acc, interval) => {
+			const start = new Date(`2021-01-01T${interval.start}`);
+			const end = new Date(`2021-01-01T${interval.end}`);
+			return acc + (end.getTime() - start.getTime());
+		}, 0);
+
+		setTotal({
+			hours: Math.floor(total / 1000 / 60 / 60),
+			minutes: Math.floor((total / 1000 / 60) % 60),
+		});
+	};
+
 	return (
 		<div>
 			<div>
@@ -24,11 +47,29 @@ const App = () => {
 					<div key={interval.id}>
 						<form>
 							<label>開始</label>
-							<input type="time" value={interval.start} />
+							<input
+								type="time"
+								value={interval.start}
+								onChange={(event) =>
+									handleChangeInterval(interval.id, {
+										...interval,
+										start: event.target.value,
+									})
+								}
+							/>
 						</form>
 						<form>
 							<label>終了</label>
-							<input type="time" value={interval.end} />
+							<input
+								type="time"
+								value={interval.end}
+								onChange={(event) =>
+									handleChangeInterval(interval.id, {
+										...interval,
+										end: event.target.value,
+									})
+								}
+							/>
 						</form>
 					</div>
 				))}
@@ -40,6 +81,16 @@ const App = () => {
 				<button type="button" onClick={handleResetIntervals}>
 					リセット
 				</button>
+			</div>
+			<div>
+				<button type="button" onClick={handleAddIntervals}>
+					計算
+				</button>
+			</div>
+			<div>
+				<p>
+					合計時間: {total.hours}時間 {total.minutes}分{" "}
+				</p>
 			</div>
 		</div>
 	);
