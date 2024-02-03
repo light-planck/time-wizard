d14e029bfa9224b4936980f4c6106405d3529f44
light planck <55218925+light-planck@users.noreply.github.com>
2024-02-03 20:37:48 +0900

    header.tsx を更新

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ export const Header = () => {
 		<div>
 			<div className="p-5">
 				<h1 className="text-2xl">Add Time Intervals</h1>
-				<p>複数の時間の区間の総和を計算できます。</p>
+				<p>時間の区間の総和を計算できます。</p>
 			</div>
 			<hr />
 		</div>
