20432da6c276bc6d79085c55004755d62040ae9a
light-planck <seseifunc16@gmail.com>
2024-02-05 11:31:24 +0900

    chore: eslintの設定

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,74 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     "eslint:recommended",
+    "plugin:import/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
     "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "import", "unused-imports"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin", // 組み込みモジュール
+          "external", // npmでインストールした外部ライブラリ
+          "internal", // 自作モジュール
+          ["parent", "sibling"],
+          "object",
+          "type",
+          "index",
+        ],
+        "newlines-between": "always", // グループ毎にで改行を入れる
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: {
+          order: "asc", // 昇順にソート
+          caseInsensitive: true, // 小文字大文字を区別する
+        },
+        pathGroups: [
+          // 指定した順番にソートされる
+          {
+            pattern: "@/components/common",
+            group: "internal",
+            position: "before",
+          },
+          {
+            pattern: "@/components/hooks",
+            group: "internal",
+            position: "before",
+          },
+        ],
+      },
+    ],
+    "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+      },
+    },
   },
 };
--- src/components/interval-input.tsx
+++ src/components/interval-input.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 
 import { TimeInterval } from "@/types";
+
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
