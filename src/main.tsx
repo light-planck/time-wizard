4aaa231d37f4b4233b9ffcfaf1719dab156b577d
light-planck <seseifunc16@gmail.com>
2024-02-13 04:04:04 +0900

    chore: 型アサーションの修正

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import * as ReactDOM from "react-dom/client";
 import App from "./app.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as Element).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
