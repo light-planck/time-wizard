d8b3c124dca3ab00eda58975e3e1bbc581e1105c
light-planck <seseifunc16@gmail.com>
2024-02-15 18:58:12 +0900

    fix: testを修正

--- src/hooks/use-local-storage/index.test.ts
+++ src/hooks/use-local-storage/index.test.ts
@@ -9,30 +9,38 @@ describe("#useLocalStorage", () => {
 
   it("初期値が設定される", () => {
     const initialValue = "initialValue";
-    const { result } = renderHook(() => useLocalStorage("key", initialValue));
-    expect(result.current.storedValue).toBe(initialValue);
+    const {
+      result: {
+        current: [storedValue],
+      },
+    } = renderHook(() => useLocalStorage("key", initialValue));
+    expect(storedValue).toBe(initialValue);
   });
 
   it("すでに値が保存されている場合、その値が取得される", () => {
     const initialValue = "initialValue";
     localStorage.setItem("key", JSON.stringify(initialValue));
-    const { result } = renderHook(() => useLocalStorage("key", "newValue"));
-    expect(result.current.storedValue).toBe(initialValue);
+    const {
+      result: {
+        current: [storedValue],
+      },
+    } = renderHook(() => useLocalStorage("key", "newValue"));
+    expect(storedValue).toBe(initialValue);
   });
 
   it("値が変更される", () => {
     const initialValue = "initialValue";
     const { result } = renderHook(() => useLocalStorage("key", initialValue));
-    expect(result.current.storedValue).toBe(initialValue);
+    expect(result.current[0]).toBe(initialValue);
 
     const newValue = "newValue";
-    expect(result.current.storedValue).not.toBe(newValue);
+    expect(result.current[0]).not.toBe(newValue);
     expect(localStorage.getItem("key")).not.toBe(JSON.stringify(newValue));
 
     act(() => {
-      result.current.setValue(newValue);
+      result.current[1](newValue);
     });
-    expect(result.current.storedValue).toBe(newValue);
+    expect(result.current[0]).toBe(newValue);
     expect(localStorage.getItem("key")).toBe(JSON.stringify(newValue));
   });
 });
