27e0a4efd28e80c987fe799c3ae5fcad51469ce7
light-planck <seseifunc16@gmail.com>
2024-02-16 00:27:21 +0900

    chore: サービス名を変更

--- src/components/layouts/header/index.tsx
+++ src/components/layouts/header/index.tsx
@@ -6,8 +6,8 @@ export const Header = () => {
       <div className="flex">
         <Logo width={100} height={100} />
         <div className="p-5">
-          <h1 className="text-2xl">Add Time Intervals</h1>
-          <p>時間の区間の総和を計算できます。</p>
+          <h1 className="text-2xl">Time Wizard</h1>
+          <p>時間の区間の合計時間を計算できます。</p>
         </div>
       </div>
       <hr />
