4cfad1ae1835839875d70af1aec43c9c5eef2a2d
light-planck <seseifunc16@gmail.com>
2024-03-16 00:52:08 +0900

    feat: time-interval-inputのテストを作成

---
+++ src/features/time/components/time-interval-input/index.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+
+import { TimeIntervalInput } from ".";
+
+describe("#TimeIntervalInput", () => {
+  const mockTimeInterval = {
+    id: crypto.randomUUID(),
+    start: "00:00",
+    end: "00:00",
+  } as const;
+
+  it("ラベルを表示する", () => {
+    render(
+      <TimeIntervalInput
+        timeInterval={mockTimeInterval}
+        onChangeTimeInterval={() => {}}
+        onDeleteTimeInterval={() => {}}
+      />,
+    );
+
+    const startTimeLabel = screen.getByLabelText("開始");
+    const endTimeLabel = screen.getByLabelText("終了");
+    expect(startTimeLabel).toBeInTheDocument();
+    expect(endTimeLabel).toBeInTheDocument();
+
+    console.log(startTimeLabel);
+  });
+});
