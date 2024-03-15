0e1dd028316b42796161b9916cf8ff14a27fd109
light-planck <seseifunc16@gmail.com>
2024-03-16 01:19:49 +0900

    chore: header, footerを調整

--- src/components/layouts/footer/index.tsx
+++ src/components/layouts/footer/index.tsx
@@ -2,18 +2,16 @@ export const Footer = () => {
   return (
     <div className="shrink-0">
       <hr />
-      <footer>
-        <div className="p-7 flex gap-5 justify-center items-center">
-          <p className="text-slate-600">
-            &copy; {new Date().getFullYear()} light planck.
-          </p>
-          <a
-            className="inline-block text-slate-600 underline hover:text-blue-500"
-            href="https://github.com/light-planck/add-time-intervals"
-          >
-            GitHub
-          </a>
-        </div>
+      <footer className="p-7 flex gap-5 justify-center items-center">
+        <p className="text-slate-600">
+          &copy; {new Date().getFullYear()} light planck.
+        </p>
+        <a
+          className="inline-block text-slate-600 underline hover:text-blue-500"
+          href="https://github.com/light-planck/add-time-intervals"
+        >
+          GitHub
+        </a>
       </footer>
     </div>
   );
--- src/components/layouts/header/index.tsx
+++ src/components/layouts/header/index.tsx
@@ -3,13 +3,13 @@ import { Logo } from "@/components/icons/logo";
 export const Header = () => {
   return (
     <div>
-      <div className="flex">
+      <header className="flex">
         <Logo width={100} height={100} />
         <div className="p-5">
           <h1 className="text-2xl">Time Wizard</h1>
           <p>時間の区間の合計時間を計算できます。</p>
         </div>
-      </div>
+      </header>
       <hr />
     </div>
   );
